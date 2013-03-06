54b61f8b14a3028d97a4a3924a444a3f502fc577
ehrickwilliam <ehrick@vista.aero>
2013-03-05 21:08:10 -0300

    Teste File

---
+++ trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/beans/FileUploadController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.cronos.beans;
+
+import javax.faces.application.FacesMessage;  
+import javax.faces.context.FacesContext;  
+  
+import org.primefaces.model.UploadedFile;  
+  
+public class FileUploadController {  
+  
+    private UploadedFile file;  
+  
+    public UploadedFile getFile() {  
+        return file;  
+    }  
+  
+    public void setFile(UploadedFile file) {  
+        this.file = file;  
+    }  
+  
+    public void upload() {  
+        if(file != null) {  
+            FacesMessage msg = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");  
+            FacesContext.getCurrentInstance().addMessage(null, msg);  
+        }  
+    }  
+} 
