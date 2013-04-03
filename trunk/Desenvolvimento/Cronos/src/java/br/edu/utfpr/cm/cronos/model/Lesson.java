c101e731d48dcf74f03aab26e77ab822c871342e
ehrickwilliam <ehrick@vista.aero>
2013-04-02 22:06:44 -0300

    Anotação onetoone

--- trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/model/Lesson.java
+++ trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/model/Lesson.java
@@ -30,7 +30,7 @@ public class Lesson implements Serializable {
     private List<Period> periods;
     @OneToOne(fetch = FetchType.EAGER)
     private Teacher teacher;
-    @OneToMany(fetch = FetchType.EAGER)
+    @OneToOne(fetch = FetchType.EAGER)
     private List<ClassRoom> classrooms;
 
     public Long getId() {
