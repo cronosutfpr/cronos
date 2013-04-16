810f86e3d5cfdc626862e1fc8a24f247cc116099
anamaciel <anacm.maciel@gmail.com>
2013-04-16 15:49:11 -0300

    cad salas

--- trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/managedbean/ClassRoomBean.java
+++ trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/managedbean/ClassRoomBean.java
@@ -5,18 +5,19 @@
 package br.edu.utfpr.cm.cronos.managedbean;
 
 import br.edu.utfpr.cm.cronos.model.ClassRoom;
-import java.awt.event.ActionEvent;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 
 /**
  *
  * @author AnaMaciel
  */
 @ManagedBean(name = "classRoomBean")
-@RequestScoped
+@SessionScoped
 public class ClassRoomBean {
 
     ClassRoom classroom;
@@ -25,6 +26,7 @@ public class ClassRoomBean {
     }
     
     public void addClassRoom(ActionEvent actionEvent){
+        System.out.println("estou aqui");
        FacesContext context = FacesContext.getCurrentInstance();  
           
         context.addMessage(null, new FacesMessage("Successful", "Hello"));  
