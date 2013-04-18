5299d5dcfb4daa5b75ee633e2946c55de70ccc4d
junior <junior@junior.(none)>
2013-04-17 22:50:38 -0300

    Criando evento de seleção de item no DataTable

--- trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/managedbean/TableClassRoom.java
+++ trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/managedbean/TableClassRoom.java
@@ -7,43 +7,67 @@ package br.edu.utfpr.cm.cronos.managedbean;
 import br.edu.utfpr.cm.cronos.daos.DaoClassRoom;
 import br.edu.utfpr.cm.cronos.daos.DaoGenerics;
 import br.edu.utfpr.cm.cronos.model.ClassRoom;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
 
 /**
  *
  * @author junior
  */
-@ManagedBean
-@SessionScoped
-public class TableClassRoom {
 
+@ManagedBean
+
+public class TableClassRoom implements Serializable{
+    
     private List<ClassRoom> classRooms;
     private ClassRoom selectedClassRoom;
-
+    private ClassRoomDataModel classRoomDataModel;
+    
     public TableClassRoom() {
         classRooms = new ArrayList<ClassRoom>();
         buscarClassRoom();
+        classRoomDataModel = new ClassRoomDataModel(classRooms);
     }
-
+    
     private void buscarClassRoom() {
         DaoGenerics<ClassRoom> daoClassRoom = new DaoClassRoom();
         classRooms = daoClassRoom.listar();
     }
-
+    
     public List<ClassRoom> getClassRooms() {
         return classRooms;
     }
-
+    
     public ClassRoom getSelectedClassRoom() {
         return selectedClassRoom;
     }
-
+    
     public void setSelectedClassRoom(ClassRoom selectedClassRoom) {
         this.selectedClassRoom = selectedClassRoom;
     }
     
+    public ClassRoomDataModel getClassRoomDataModel() {
+        return classRoomDataModel;
+    }
+    
+    public void onRowSelect(SelectEvent event) {
+        
+        FacesMessage msg = new FacesMessage("Sala selecionada", String.valueOf(((ClassRoom) event.getObject()).getId()));
+        
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+    public void onRowUnselect(UnselectEvent event) {
+        FacesMessage msg = new FacesMessage("Sala não selecionada", String.valueOf(((ClassRoom) event.getObject()).getId()));
+        
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
     
 }
