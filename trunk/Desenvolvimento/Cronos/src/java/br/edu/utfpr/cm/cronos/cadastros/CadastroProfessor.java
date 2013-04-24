f830c873cdbd9ab1b666b22a8ed5a74e05c562ee
ehrickwilliam <ehrick@vista.aero>
2013-04-24 19:35:02 -0300

    cadastro de professores com problema

--- trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/cadastros/CadastroProfessor.java
+++ trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/cadastros/CadastroProfessor.java
@@ -24,6 +24,7 @@ import org.primefaces.component.datatable.feature.DataTableFeature;
 @ManagedBean(name = "cadastroProfessor")
 @SessionScoped
 public class CadastroProfessor implements Serializable {
+    
 
     private String siape;
 
