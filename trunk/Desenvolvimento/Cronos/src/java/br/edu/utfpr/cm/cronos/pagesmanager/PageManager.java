db4de50dd6fd7d13a1db1d04a2c24efdedaf44d3
junior <junior@junior.(none)>
2013-04-29 20:28:47 -0300

    Corrigindo tela de listagem de reservas

--- trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/pagesmanager/PageManager.java
+++ trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/pagesmanager/PageManager.java
@@ -10,9 +10,8 @@ public class PageManager implements Serializable {
 
     public PageManager() {
     }
-    private String paginaAtiva = "./pages/home.xhtml";    
+    private String paginaAtiva = "./pages/home.xhtml";
 
-    
     public String getPaginaAtiva() {
         return paginaAtiva;
     }
@@ -20,48 +19,54 @@ public class PageManager implements Serializable {
     public void setPaginaAtiva(String paginaAtiva) {
         this.paginaAtiva = paginaAtiva;
     }
-    
+
     public void setPg1() {
         this.paginaAtiva = "./pages/cad_salas.xhtml";
     }
-    
+
+    public void setPagListaSalasReserva() {
+        this.paginaAtiva = "./pages/lista_salas_reserva.xhtml";
+    }
+
     public void setPagListaSalas() {
         this.paginaAtiva = "./pages/lista_salas.xhtml";
     }
-    
+
     public void setPagListaProfs() {
         this.paginaAtiva = "./pages/lista_professores.xhtml";
     }
-    
+
     public void setPagHorarioSalas() {
         this.paginaAtiva = "./pages/horario_sala.xhtml";
     }
-    
-    
+
+    public void setPagListaReservasUsuario(){
+        this.paginaAtiva = "./pages/lista_reservas_usuario.xhtml";
+    }
     public void setPg4() {
         this.paginaAtiva = "./pages/add_professores.xhtml";
     }
-    
+
     public void setPg5() {
         this.paginaAtiva = "./pages/cad_disciplinas.xhtml";
     }
-    
-       
+
     public String getPg1() {
         return this.paginaAtiva;
     }
-    
+
     public String getPg2() {
         return this.paginaAtiva;
     }
-    
+
     public String getPg3() {
         return this.paginaAtiva;
     }
+
     public String getPg4() {
         return this.paginaAtiva;
     }
-    
+
     public String getPg5() {
         return this.paginaAtiva;
     }
