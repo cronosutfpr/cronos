82ce03b521058d33d3b68a5384ca12848ac7fa2e
junior <junior@junior.(none)>
2013-05-05 21:15:35 -0300

    Creando bean para cadastrar reserva

--- trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/controller/ScheduleController.java
+++ trunk/Desenvolvimento/Cronos/src/java/br/edu/utfpr/cm/cronos/controller/ScheduleController.java
@@ -25,7 +25,7 @@ public class ScheduleController implements Serializable {
 
     public ScheduleController() {
         eventModel = new DefaultScheduleModel();
-        eventModel.addEvent(new DefaultScheduleEvent("Champions League Match", previousDay8Pm(), previousDay11Pm()));
+        eventModel.addEvent(new DefaultScheduleEvent("teste", previousDay8Pm(), previousDay11Pm()));
         eventModel.addEvent(new DefaultScheduleEvent("Birthday Party", today1Pm(), today6Pm()));
         eventModel.addEvent(new DefaultScheduleEvent("Breakfast at Tiffanys", nextDay9Am(), nextDay11Am()));
         eventModel.addEvent(new DefaultScheduleEvent("Plant the new garden stuff", theDayAfter3Pm(), fourDaysLater3pm()));
